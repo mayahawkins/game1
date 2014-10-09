01107e310d23324d94c9e9fe345bf548d4f666cd
mayahawkins <maya.t.hawkins@gmail.com>
2014-10-08 20:04:30 -0400

    starting

--- class fallingPerfectly.java
+++ class fallingPerfectly.java
@@ -1,7 +1,29 @@
-class fallingPerfectly{
-	
+import javalib.funworld.*;
+import javalib.worldimages.*;
+import javalibworldcanvas.*;
+import javalib.colors.*
+
+class FallingPerfectly{
+}
 
 
 
+class PlayerBlock{
+	int length;
+	int width;
+	IColor color;
+	int area;
+
+	PlayerBlock(int length; int width, IColor color){
+		this.length = length;
+		this.width = width;
+		this.color = col;
+		this.area = this.length * this.width;
+	}
+	WorldImage playerBlocklImage(){
+		return new RectangleImage(this.length, this.width, this.color)
+	} 
 	
 }
+
+
