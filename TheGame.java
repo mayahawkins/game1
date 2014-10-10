d8eaad7dcf0e03aeb972f1bdaf058faa2e43ac20
mayahawkins <maya.t.hawkins@gmail.com>
2014-10-10 05:43:56 -0400

    More of Cat and Dead Mouse

--- TheGame.java
+++ TheGame.java
@@ -5,11 +5,11 @@ import javalib.colors.*;
 
 
 class Mouse{
-	public Posn pinhole;
-	public int length;
-	public int width;
-	public IColor color;
-	public int area;
+	private Posn pinhole;
+	private int length;
+	private int width;
+	private IColor color;
+	private int area;
 
 	public Mouse(Posn pinhole, int length, int width, IColor color){
 		this.pinhole = pinhole;
@@ -49,15 +49,15 @@ class Mouse{
 		return (this.pinhole.x + (this.width / 2)) > 500
 		||     (this.pinhole.x - (this.width / 2)) < 0
 		||     (this.pinhole.y + (this.height / 2)) > 500
-		||     (this.pinhole.y - (this.height / 2)) < 0
+		||     (this.pinhole.y - (this.height / 2)) < 0;
 	}
 }
 
 class StickyPaper {
-	Posn sTPinhole;
-	int sTHeight;
-	int sTWidth;
-	IColor sTColor;
+	private Posn sTPinhole;
+	private int sTHeight;
+	private int sTWidth;
+	private IColor sTColor;
 
 	StickyPaper(Posn sTPinhole, int sTHeight, int sTWidth, IColor sTColor){
 	this.sTPinhole = sTPinhole;
@@ -72,16 +72,19 @@ class StickyPaper {
 	}
 
 	public boolean stuckHuh(Mouse mousePlayer) {
-		mousePlayer.pinhole.x
+		return (mousePlayer.pinhole.x =< (500 - this.sTPinhole.x + (this.sTWidth / 2)))
+			&& (mousePlayer.pinhole.x => (this.sTPinhole.x - (this.sTWidth / 2)))_
+			&& (mousePlayer.pinhole.y =< (500 - this.sTPinhole.y + (this.sTHeight / 2)))
+			&& (mousePlayer.pinhole.y => (this.sTPinhole.y - (this.sTHeight / 2)));
 	}
 
 }
 
 class Cat {
-	Posn cPinhole;
-	int cHeight;
-	int cWidth;
-	IColor cColor;
+	private Posn cPinhole;
+	private int cHeight;
+	private int cWidth;
+	private IColor cColor;
 
 	Cat(Posn cPinhole, int cHeight, int cWidth, IColor cColor){
 	this.cPinhole = cPinhole;
@@ -94,6 +97,47 @@ class Cat {
 		return new RectangleImage(this.cPinhole, this.cLength,
 								 this.cWidth, this.cColor);
 	}
+
+	public boolean feedCat(Mouse mousePlayer, String ke) {
+		return (mousePlayer.pinhole.x =< (500 - this.cPinhole.x + (this.cWidth / 2)))
+			&& (mousePlayer.pinhole.x => (this.cPinhole.x - (this.cWidth / 2)))_
+			&& (mousePlayer.pinhole.y =< (500 - this.cPinhole.y + (this.cHeight / 2)))
+			&& (mousePlayer.pinhole.y => (this.cPinhole.y - (this.cHeight / 2)))
+			&& (ke.equals "right");
+	}
+
+	public boolean wrongFeeding(Mouse mousePlayer, Sting ke){
+		return (mousePlayer.pinhole.x =< (500 - this.cPinhole.x + (this.cWidth / 2)))
+			&& (mousePlayer.pinhole.x => (this.cPinhole.x - (this.cWidth / 2)))_
+			&& (mousePlayer.pinhole.y =< (500 - this.cPinhole.y + (this.cHeight / 2)))
+			&& (mousePlayer.pinhole.y => (this.cPinhole.y - (this.cHeight / 2)))
+			&& ((ke.equals "up") || (ke.equals "down"));
+	}
+}
+
+class DeadMouse {
+	private Posn dPinhole;
+	private int dLength;
+	private int dWidth;
+	private IColor dColor;
+
+	public DeadMouse(Posn dPinhole, int dLength, int dWidth, IColor dColor){
+		this.dPinhole = dPinhole;
+		this.dLength = dLength;
+		this.dWidth = dWidth;
+		this.dColor = dColor;
+	}
+	public WorldImage deadMouseImage(){
+		return new RectangleImage(this.dPinhole, this.dLength, this.dWidth, this.dColor);
+	}
+
+	public boolean lawsOfLife(Mouse mousePlayer) {
+		return (mousePlayer.pinhole.x =< (500 - this.dPinhole.x + (this.dWidth / 2)))
+			&& (mousePlayer.pinhole.x => (this.dPinhole.x - (this.dWidth / 2)))_
+			&& (mousePlayer.pinhole.y =< (500 - this.dPinhole.y + (this.dHeight / 2)))
+			&& (mousePlayer.pinhole.y => (this.dPinhole.y - (this.dHeight / 2)));
+	}
+
 }
 
 
@@ -101,10 +145,10 @@ class Cat {
 
 public class TheGame extends World {
 	int worldHeight = 500;
-	int worldWidth = 300;
+	int worldWidth = 500;
  	PlayerBlock playerBlock;
 
- 	public TheGame(PlayerBlock playerBlock){
+ 	public TheGame(Mouse mousePlayer){
  		super();
  		this.playerBlock = playerBlock;
  	}
