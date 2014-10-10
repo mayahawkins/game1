605e7476b924198ce2325d4e01b759569e88a18f
mayahawkins <maya.t.hawkins@gmail.com>
2014-10-10 05:15:18 -0400

    Changing up the game more...
    made the players some more, making parameters for mousetraps and Cat

--- TheGame.java
+++ TheGame.java
@@ -4,24 +4,24 @@ import javalibworldcanvas.*;
 import javalib.colors.*;
 
 
-class PlayerBlock{
-	Posn pinhole;
-	int length;
-	int width;
-	IColor color;
-	int area;
+class Mouse{
+	public Posn pinhole;
+	public int length;
+	public int width;
+	public IColor color;
+	public int area;
 
-	PlayerBlock(int length, int width, IColor color){
+	public Mouse(Posn pinhole, int length, int width, IColor color){
 		this.pinhole = pinhole;
 		this.length = length;
 		this.width = width;
 		this.color = color;
 		this.area = this.length * this.width;
 	}
-	WorldImage playerBlocklImage(){
-		return new RectangleImage(this. pinhole, this.length, this.width, this.color);
+	public WorldImage mouseImage(){
+		return new RectangleImage(this.pinhole, this.length, this.width, this.color);
 	}
-	 public PlayerBlock moveBlock(String ke){
+	 public Mouse moveMouse(String ke){
 	 	if (ke.equals("right")){
 	 		return new RectangleImage(new Posn(this.pinhole.x + 10, this.pinhole.y),
 	 			this.length, this.width, this.color);
@@ -41,16 +41,63 @@ class PlayerBlock{
 	 	else {
 	 		return this;
 	 	}
-
 	}
-	public boolean outOfBounds(int width, in height){
-		return (this.pinhole.x + (this.width / 2)) > width
+	public void mousePlacer(int x, int y) {
+		this.pinhole = new Posn(x, y);
+	}
+	public boolean outOfBounds(){
+		return (this.pinhole.x + (this.width / 2)) > 500
 		||     (this.pinhole.x - (this.width / 2)) < 0
-		||     (this.pinhole.y + (this.height / 2)) > height
+		||     (this.pinhole.y + (this.height / 2)) > 500
 		||     (this.pinhole.y - (this.height / 2)) < 0
+	}
+}
+
+class StickyPaper {
+	Posn sTPinhole;
+	int sTHeight;
+	int sTWidth;
+	IColor sTColor;
+
+	StickyPaper(Posn sTPinhole, int sTHeight, int sTWidth, IColor sTColor){
+	this.sTPinhole = sTPinhole;
+	this.sTHeight = sTHeight;
+	this.sTWidth = sTWidth;
+	this.sTColor = sTColor;	
+	}
+
+	WorldImage StickyPaperImage(){
+		return new RectangleImage(this.sTPinhole, this.sTLength,
+								 this.sTWidth, this.sTColor);
+	}
+
+	public boolean stuckHuh(Mouse mousePlayer) {
+		mousePlayer.pinhole.x
 	}
 
 }
+
+class Cat {
+	Posn cPinhole;
+	int cHeight;
+	int cWidth;
+	IColor cColor;
+
+	Cat(Posn cPinhole, int cHeight, int cWidth, IColor cColor){
+	this.cPinhole = cPinhole;
+	this.cHeight = cHeight;
+	this.cWidth = cWidth;
+	this.cColor = cColor;	
+	}
+
+	WorldImage CatImage(){
+		return new RectangleImage(this.cPinhole, this.cLength,
+								 this.cWidth, this.cColor);
+	}
+}
+
+
+
 
 public class TheGame extends World {
 	int worldHeight = 500;
